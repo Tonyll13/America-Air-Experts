96abb0ad5d98e6c27c5d18a32a90e9085c3d6c3f
Tonyll13 <shlomilevy34@gmail.com>
2026-01-05 20:13:37 -0500

    Fix prerender error (MetaPixelPageView)

--- app/MetaPixelPageView.tsx
+++ app/MetaPixelPageView.tsx
@@ -1,20 +1,14 @@
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 export default function MetaPixelPageView() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
-    // מחכה שהפיקסל יהיה קיים
-    // @ts-ignore
-    if (typeof window !== "undefined" && (window as any).fbq) {
-      // @ts-ignore
-      (window as any).fbq("track", "PageView");
-    }
-  }, [pathname, searchParams]);
+    (window as any).fbq?.("track", "PageView");
+  }, [pathname]);
 
   return null;
 }
